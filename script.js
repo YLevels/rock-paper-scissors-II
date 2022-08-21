1c18a6e4265539e16542e5dafbe54645eadddd05
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-21 16:28:19 -0400

    game() works!

--- script.js
+++ script.js
@@ -18,24 +18,63 @@ function playRound (playerSelection, computerSelection) {
     let playerMove = playerSelection.toLowerCase();
     let computerMove = computerSelection;
     let message = "";
+    let winner = "";
 
     if (playerMove === computerMove) {
         message = "You chose " + playerMove + ". The computer chose " + computerMove + ". It is a tie.";
+        winner = "tie";
     }
 
     if ((playerMove === "rock" && computerMove === "scissors") || (playerMove === "paper" && computerMove === "rock") || (playerMove === "scissors" && computerMove === "paper")) {
         message = "You chose " + playerMove + ". The computer chose " + computerMove + ". You win!";
+        winner = "player";
     }
 
     if ((playerMove === "rock" && computerMove === "paper") || (playerMove === "paper" && computerMove === "scissors") || (playerMove === "scissors" && computerMove === "rock")) {
         message = "You chose " + playerMove + ". The computer chose " + computerMove + ". You lose.";
+        winner = "computer";
     }
 
-    return message;
+    return winner;
 }
 
-const playerSelection = "scissors";
-console.log("The player selected: " + playerSelection);
-const computerSelection = getComputerChoice();
-console.log("The computer selected: " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameWinner = "";
+    
+    for (let i = 0; i < 5; i++) {
+        let question = prompt("Do you choose rock, paper, or scissors?");
+        let roundWinner = playRound(question.toLowerCase(),getComputerChoice());
+        
+        if (roundWinner === "tie") {
+            console.log("this is a tie")
+        }
+        else if (roundWinner === "player") {
+            playerScore++;
+        }
+        else {
+            computerScore++;
+        }
+        console.log(roundWinner + " won this round");
+    }
+
+    if (playerScore === computerScore) {
+        gameWinner = "It is a tie";
+    }
+    else if (playerScore > computerScore) {
+        gameWinner = "player wins!";
+    }
+    else {
+        gameWinner = "computer wins!";
+    }
+
+    return gameWinner;
+}
+
+// const playerSelection = "scissors";
+// console.log("The player selected: " + playerSelection);
+// const computerSelection = getComputerChoice();
+// console.log("The computer selected: " + computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+console.log(game());
