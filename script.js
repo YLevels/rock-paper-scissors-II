c5218841275df961c40076b2899e082b37b786f1
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-21 10:59:56 -0400

    getComputerChoice is running

---
+++ script.js
@@ -0,0 +1,16 @@
+//randomly returns either 'rock', 'paper', or 'scissors'
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerChoice = "";
+  
+    if (randomNumber === 1) {
+      computerChoice = "rock";
+    } else if (randomNumber === 2) {
+      computerChoice = "paper";
+    } else {
+      computerChoice = "scissors";
+    }
+    return computerChoice;
+  }
+
+  console.log(getComputerChoice());
