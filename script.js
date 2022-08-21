a433416f6e1e42af9e090bfdbc17f54ae2b9a646
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-21 13:46:33 -0400

    playRound() works!

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ function getComputerChoice () {
     return computerChoice;
   }
 
-  console.log(getComputerChoice());
+//plays a single round of the game
+function playRound (playerSelection, computerSelection) {
+    let playerMove = playerSelection.toLowerCase();
+    let computerMove = computerSelection;
+    let message = "";
+
+    if (playerMove === computerMove) {
+        message = "You chose " + playerMove + ". The computer chose " + computerMove + ". It is a tie.";
+    }
+
+    if ((playerMove === "rock" && computerMove === "scissors") || (playerMove === "paper" && computerMove === "rock") || (playerMove === "scissors" && computerMove === "paper")) {
+        message = "You chose " + playerMove + ". The computer chose " + computerMove + ". You win!";
+    }
+
+    if ((playerMove === "rock" && computerMove === "paper") || (playerMove === "paper" && computerMove === "scissors") || (playerMove === "scissors" && computerMove === "rock")) {
+        message = "You chose " + playerMove + ". The computer chose " + computerMove + ". You lose.";
+    }
+
+    return message;
+}
+
+const playerSelection = "scissors";
+console.log("The player selected: " + playerSelection);
+const computerSelection = getComputerChoice();
+console.log("The computer selected: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
